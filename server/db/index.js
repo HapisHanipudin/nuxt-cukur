21e2af444070a84a37ee95f1121750fb7bb9cbd3
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-04-08 13:17:20 +0700

    using prisma accelerate

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
-import Prisma from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new Prisma.PrismaClient();
-
+const prisma = new PrismaClient().$extends(withAccelerate());
 export { prisma };
