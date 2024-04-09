f7293c34d2e7a9e073b167c8fec2bddb8c427639
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-04-09 18:40:48 +0700

    homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,9 @@
-<template>ini root</template>
+<template>
+  <div class="container">
+    <h1 class="text-gold-700 font-black font-sans text-3xl">Welcome To Dream Cuts</h1>
+    <div class="mt-8">
+      <span class="text-gray-500">Ok, jadi apa itu dreamcuts?</span>
+      <p>Jadi, Dream Cuts adalah sistem yang akan membantu OSQBS untuk mengatur dan mendata pencukuran di Sekolah Impian.</p>
+    </div>
+  </div>
+</template>
