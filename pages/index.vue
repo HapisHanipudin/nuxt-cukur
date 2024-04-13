99799465b22f0db93ecc6860c0810cefaee05ba6
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-04-13 15:36:42 +0700

    idk

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,15 @@
   <div class="container">
     <h1 class="text-gold-700 font-black font-sans text-3xl">Welcome To Dream Cuts</h1>
     <div class="mt-8">
-      <span class="text-gray-500">Ok, jadi apa itu dreamcuts?</span>
-      <p>Jadi, Dream Cuts adalah sistem yang akan membantu OSQBS untuk mengatur dan mendata pencukuran di Sekolah Impian.</p>
+      <span class="text-gray-400 text-xl">Ok, jadi apa itu Dream Cuts?</span>
+      <p class="text-2xl">Dream Cuts adalah sebuah sistem yang akan membantu OSQBS untuk mengatur dan mendata pencukuran di Sekolah Impian.</p>
+      <p class="text-2xl">Sistem ini dibuat menggunakan NodeJS, Nuxt.js, Prisma, dan menggunakan database Postgresql</p>
+    </div>
+    <h1 class="text-gold-700 font-black font-sans text-3xl">Alur Dream Cuts</h1>
+    <div class="mt-8">
+      <span class="text-gray-400 text-xl">Gimana alurnya ?</span>
+      <p class="text-2xl">Dream Cuts adalah sebuah sistem yang akan membantu OSQBS untuk mengatur dan mendata pencukuran di Sekolah Impian.</p>
+      <p class="text-2xl">Sistem ini dibuat menggunakan NodeJS, Nuxt.js, Prisma, dan menggunakan database Postgresql</p>
     </div>
   </div>
 </template>
